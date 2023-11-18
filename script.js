326276b253f23773ba2bbd9c223a928ae332b01d
Vn0m <juanpapolo123@gmail.com>
2023-11-17 21:06:25 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function getRandomColor(){
+    const COLORS = [
+        "#a2d1fc",
+        "#ffc2cd",
+        "#97aedc",
+        "#d967ae",
+        "#b7cf8e",
+        "#d5edc2",
+    ];
+
+    return COLORS[Math.floor(Math.random() * COLORS.length)];
+}
+
+
+function sendNote(){
+    /*
+    1.take the note from the input box
+    2.clear the note from the input box
+    3.add the note to the bottom of our list
+    */
+
+    const inputElement = document.getElementById('input');
+    const noteContent = inputElement.value;
+    inputElement.value = "";
+
+    let newNote = document.createElement('li');
+    newNote.style = "height: 150px;";
+    newNote.style.backgroundColor = getRandomColor();
+    newNote.classList.add('container');
+    newNote.classList.add("inner-container");
+    newNote.innerHTML = noteContent;
+    let allNotes = document.getElementById('notes');
+    allNotes.appendChild(newNote);
+
+
+}
