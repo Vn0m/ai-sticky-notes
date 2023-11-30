e5ea9c74f52215a48bc6cd1208ead2034db271fa
Vn0m <juanpapolo123@gmail.com>
2023-11-30 17:26:19 -0500

    integrate chat gpt into notes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const API_KEY = '';
+
 function getRandomColor(){
     const COLORS = [
         "#a2d1fc",
@@ -12,8 +14,7 @@ function getRandomColor(){
 }
       
 
-
-function sendNote(){
+async function sendNote(){
     /*
     1.take the note from the input box
     2.clear the note from the input box
@@ -28,10 +29,32 @@ function sendNote(){
     newNote.style = "height: 150px";
     newNote.style.backgroundColor = getRandomColor();
     newNote.classList.add('container');
-    
-    newNote.innerHTML = noteContent;
-    let allNotes = document.getElementById('notes');
-    allNotes.appendChild(newNote);
 
+    const data = {
+        model: 'gpt-3.5-turbo',
+        messages: [{role: 'assistant', content: `Turn this into a dad joke: ${noteContent}`}],
+        max_tokens: 50
+    }
 
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_KEY}`
+    }
+
+    // fetch(RESOURCE -> from where,OPTION -> additional info like key, format, etc.)
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data)
+    });
+
+    if(response.ok){
+        const result =  await response.json();
+        const generatedText = result.choices[0].message.content;
+        newNote.innerHTML = generatedText;
+        let allNotes = document.getElementById('notes');
+        allNotes.appendChild(newNote);
+    } else {
+        console.error("Error!");
+    }
 }
